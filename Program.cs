a712a68f56df85e2e059d30cc3c5e141de4a559f
andrepresys <andre.presys@gmail.com>
2021-03-19 11:20:45 -0300

    Simple Test

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TesteLog4Net
+{
+    class Program
+    {                                                                                                                     //Aqui no System.Reflexion, ele pega o método atual e declara o seu tipo
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Main(string[] args)
+        {
+
+            log.Info("Bem vindos á nossa Demo de Log4Net!");
+            Console.Write("Digite um numero: ");
+            double num; 
+            bool sucesso = double.TryParse(Console.ReadLine(), out num);
+            if (!sucesso)
+                log.Error("Número digitado incorretamente!");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
